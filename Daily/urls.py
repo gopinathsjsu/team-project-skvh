aa4b7adaee167b0d7f1104130135182a1f631126
Kunjal Shah <kunjalprashant.shah@sjsu.edu>
2023-03-11 21:13:24 -0700

    urls added for checkin checkout

--- Daily/urls.py
+++ Daily/urls.py
@@ -4,4 +4,7 @@ from Daily import views
 urlpatterns = [
     url(r'^checkin', views.check_in),
     url(r'^checkout', views.check_out),
+    url(r'log_activity', views.log_activity),
+    url(r'get_checkins', views.check_in_counts),
+    url(r'get_hours', views.hours_count),
 ]
