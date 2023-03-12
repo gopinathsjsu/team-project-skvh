4c137b6074c96e264285e6638c65db5cc06f857a
Vandana Patel <vandana.patel@sjsu.edu>
2023-03-11 21:13:24 -0700

    added class model

--- Business/models.py
+++ Business/models.py
@@ -8,3 +8,20 @@ class Instructor(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Class(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    strength = models.IntegerField(default=20)
+    instructor = models.ForeignKey(
+        'Business.Instructor', on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    location = models.ForeignKey(
+        'Gym.Location', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+    def spots_left(self):
+        enrolled_count = Enrolled.objects.filter(enrolled_class=self).count()
+        return max(self.strength - enrolled_count, 0)
