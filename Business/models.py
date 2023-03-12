ab3d1aeb63fb557def02ed3b8f0b2ba93421a655
Vandana Patel <vandana.patel@sjsu.edu>
2023-03-11 21:13:24 -0700

    added instructor model

--- Business/models.py
+++ Business/models.py
@@ -2,3 +2,9 @@ from django.db import models
 import datetime
 
 # Create your models here.
+class Instructor(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=10, null=True, blank=True, unique=True)
+
+    def __str__(self):
+        return self.name
