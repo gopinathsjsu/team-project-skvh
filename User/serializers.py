fc1e92e2725fe0655d89774ab55f177bf8bcc432
Kunjal Shah <kunjalprashant.shah@sjsu.edu>
2023-03-25 21:13:24 -0700

    created User serializer file

---
+++ User/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework.serializers import ModelSerializer
+from User.models import User, Enrolled
+from Business.serializers import ClassSerializer
+from rest_framework import serializers
+from django.utils import timezone
+
+class UserSerializer(ModelSerializer):
+    role = serializers.SerializerMethodField()
+    class Meta:
+        model = User
+        fields=('id', 'fname', 'lname', 'email', 'phone', 'role',)
+
+    def get_role(self, obj):
+        if obj.role == 0:
+            return 'admin'
+        elif obj.role == 1 and obj.expiry_date and obj.expiry_date > timezone.now():
+            return 'member'
+        else:
+            return 'non-member'
