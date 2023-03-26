4efe631f50a780ccbdb64eae8f237d99d2b0f226
Hardik Ghori <kidarhpatel@gmail.com>
2023-03-25 21:13:24 -0700

    added Plans component

---
+++ frontend/src/components/Plans.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// import './Plans.css';
+
+function Plans({ plans }) {
+  return (<>
+    <h2 className="text-center my-5">Memberships</h2>
+    <div className="container">
+    <div className="row row-cols-1 row-cols-md-5 g-4">
+      {plans.map((plan) => (
+        <div className="col" key={plan.name}>
+          <div className="card h-100">
+            <div className="card-header" style={{backgroundColor:'#24776e'}}>{plan.name}</div>
+            <div className="card-body">
+              {/* <h5 className="card-title">{plan.name}</h5> */}
+              <p className="card-text">${plan.price} ({plan.duration} days)</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+    </div>
+    </>
+  );
+  
+}
+
+export default Plans;
