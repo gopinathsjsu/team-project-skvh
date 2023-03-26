dd144a5e33302f261bad41a1fe6c01fd9f3bf3d8
Kunjal Shah <kunjalprashant.shah@sjsu.edu>
2023-03-25 21:13:24 -0700

    added Daily models

--- Daily/models.py
+++ Daily/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+from Gym.models import Equipment
 
 # Create your models here.
+class Entry(models.Model):
+    user = models.ForeignKey(
+        'User.User', on_delete=models.DO_NOTHING)
+    location = models.ForeignKey(
+        'Gym.Location', on_delete=models.DO_NOTHING)
+    checkin_time = models.DateTimeField(default=timezone.now)
+    checkout_time = models.DateTimeField(null=True, blank=True)
+
+    @property
+    def has_checked_out(self):
+        if self.checkout_time:
+            return True
+        return False
+
+    @property
+    def duration(self):
+        if not self.has_checked_out:
+            return None
+        seconds = (self.checkout_time - self.checkin_time).total_seconds()
+        hours = int(seconds // 3600)
+        minutes = int((seconds % 3600) // 60)
+        return seconds
