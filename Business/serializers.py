f2fd6517eb302784fff249ec78f08208d08f4644
Vandana Patel <vandana.patel@sjsu.edu>
2023-04-22 21:13:24 -0700

    added class serializer

--- Business/serializers.py
+++ Business/serializers.py
@@ -7,3 +7,12 @@ class PlanSerializer(ModelSerializer):
     class Meta:
         model = Plan
         fields = '__all__'
+
+
+class ClassSerializer(ModelSerializer):
+    instructor = serializers.CharField(source='instructor.name')
+    location = serializers.CharField(source='location.name')
+
+    class Meta:
+        model = Class
+        fields = ('id', 'name', 'strength', 'time', 'instructor', 'spots_left', 'location')
